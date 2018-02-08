c8f46eec75fe88420e4ec775fdd46be91622c7b4
罗梦婕 <luomj@metersbonwe.com>
2018-02-08 17:59:29 +0800

    sort init

--- pages/sort/sort.js
+++ pages/sort/sort.js
@@ -5,14 +5,50 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    firstLevCates:'',
+    firstCate0:{
+      title:'',
+      secondCates:{
+        secondCate0:{
+          title:'',
+          banner:'',
+          children:[
+            {name:'',image:''}
+          ]
+        }
+      }
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var _this = this;
+    //一级目录
+    wx.request({
+      url: 'http://m.banggo.com/list-category/getFirstLevCates.shtml',
+      success:function(res){
+        var data = res.data.data;
+        _this.setData({firstLevCates:data});
+      }
+    });
+    // 一级目录第一项初始数据
+    wx.request({
+
+      url: 'http://m.banggo.com/list-category/getCateRelChild.shtml?cate_id=492',
+      success:function(res){
+
+        // debugger;
+        // _this.setData({
+
+        // })
+      }
+
+    });
+    var query = wx.createSelectorQuery();
+    query.select('.sortScroll').;
+    debugger;
   },
 
   /**
